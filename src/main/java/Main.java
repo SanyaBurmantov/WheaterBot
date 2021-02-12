d0a34b473fd06ecab921c667a90c0091e8b3975c
Sanya Burmantov <SanyaBurmantov@gmail.com>
2021-02-13 01:03:38 +0300

    first commit jdk

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("let's get it!");
+    public static void main(String[] args) throws TelegramApiException {
+
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+
+            botsApi.registerBot(new WeatherTestApplication_bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+   List<Integer> list = new LinkedList<Integer>();
+
 }
--- src/main/java/WeatherTestApplication_bot.java
+++ src/main/java/WeatherTestApplication_bot.java
@@ -1,5 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.SQLOutput;
 
 public class WeatherTestApplication_bot extends TelegramLongPollingBot {
 
@@ -12,6 +16,17 @@ public class WeatherTestApplication_bot extends TelegramLongPollingBot {
     }
 
     public void onUpdateReceived(Update update) {
+
+      //  System.out.println(update.getMessage().getText());
+        String command= update.getMessage().getText();
+        SendMessage message = new SendMessage();
+        System.out.println(update.getMessage().getText());
+        if(command.equals("/базарь")){
+            System.out.println(update.getMessage().getFrom().getFirstName());
+            System.out.println(update.getMessage().getText());
+            message.setText(update.getMessage().getText());
+
+        }
 
     }
 }
