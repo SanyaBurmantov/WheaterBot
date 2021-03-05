f5ea4c297b3fcca59527ba67ce40bf1c0a346cbb
Sanya Burmantov <SanyaBurmantov@gmail.com>
2021-03-05 22:57:36 +0300

    first commit jdk

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import telegram.StartBot;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-        StartBot.Startbot();
+        StartBot.StartBot();
     }
 
 }
--- src/main/java/telegram/StartBot.java
+++ src/main/java/telegram/StartBot.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class StartBot {
-public static void Startbot() throws TelegramApiException{
+public static void StartBot() throws TelegramApiException{
 
     TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
     try {
